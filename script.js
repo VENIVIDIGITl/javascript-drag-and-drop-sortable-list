a47253a0fad5aa8e68af60414fad3dc875c61c31
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-05-10 18:25:44 +0300

    separate create list item logic into its own createListItem() function, add scrambleArray() function to randomize the order of programmingLanguages array, in order to create the list of list items in a random order

--- script.js
+++ script.js
@@ -16,44 +16,60 @@ const programmingLanguages = [
 ];
 
 
-// Store listitems
+// Store list items
 const listItems = [];
 
 let dragStartIndex;
 
 createList();
 
+
+function scrambleArray(array) {
+  return array
+    .map(item => ({ value: item, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(obj => obj.value);
+}
+
+
+function createListItem(text, index) {
+  const listItem = document.createElement('li');
+  listItem.setAttribute('data-index', index);
+
+  const span = document.createElement('span');
+  span.classList.add('number');
+  span.textContent = index + 1;
+
+  const div = document.createElement('div');
+  div.setAttribute('draggable', 'true');
+  div.classList.add('draggable');
+  
+  const p = document.createElement('p');
+  p.classList.add('language-name');
+  p.textContent = text;
+
+  const icon = document.createElement('i');
+  icon.classList.add('fas', 'fa-grip-lines');
+
+  div.appendChild(p);
+  div.appendChild(icon);
+  listItem.appendChild(span);
+  listItem.appendChild(div);
+
+  return listItem;
+}
+
+
 // Insert list items into DOM
 function createList() {
-  [...programmingLanguages].forEach((language, index) => {
-    // Create List Item
-    const listItem = document.createElement('li');
-    listItem.setAttribute('data-index', index);
-
-    const span = document.createElement('span');
-    span.classList.add('number');
-    span.textContent = index + 1;
-
-    const div = document.createElement('div');
-    div.setAttribute('draggable', 'true');
-    div.classList.add('draggable');
-    
-    const p = document.createElement('p');
-    p.classList.add('language-name');
-    p.textContent = language;
-
-    const icon = document.createElement('i');
-    icon.classList.add('fas', 'fa-grip-lines');
-
-    div.appendChild(p);
-    div.appendChild(icon);
-    listItem.appendChild(span);
-    listItem.appendChild(div);
-
-    // Push into array
-    listItems.push(listItem);
-
-    // Insert into DOM
-    draggableList.appendChild(listItem);
-  });
+  const languages = scrambleArray(programmingLanguages);
+  languages
+    .forEach((language, index) => {
+      // Create List Item
+      const listItem = createListItem(language, index);
+      // Push into list items array
+      listItems.push(listItem);
+      // Insert into DOM
+      draggableList.appendChild(listItem);
+    });
 }
